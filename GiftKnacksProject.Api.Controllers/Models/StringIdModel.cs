64e2395e16201a60ab96b26c4f22db13c1116988
Andrew Kozak <dimaivanov1@mail.ru>
2016-04-02 20:13:31 +0300

    Rename json input property

--- GiftKnacksProject.Api.Controllers/Models/StringIdModel.cs
+++ GiftKnacksProject.Api.Controllers/Models/StringIdModel.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace GiftKnacksProject.Api.Controllers.Models
 {
     public class StringIdModel
     {
+        [JsonProperty("id")]
         public string Id { get; set; }
     }
 }
